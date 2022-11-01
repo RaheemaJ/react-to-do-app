c0abfe3985e0502ab51c5b233535e9c1e0c6a989
Raheema Jannah <ed202rj@leeds.ac.uk>
2022-11-01 20:17:48 +0000

    initial files

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+      <div className="todo"> 
+        <h1>Tasks</h1>
+          <div className="flex-container">
+            <h2>date</h2>
+          </div>
+      </div>
+  )
+}
+
+export default App
