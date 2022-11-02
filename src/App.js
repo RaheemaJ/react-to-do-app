45c110f93899e364bb1bad86d4b39b962c4b2428
Raheema Jannah <ed202rj@leeds.ac.uk>
2022-11-02 13:45:42 +0000

    some initial styling completed

--- src/App.js
+++ src/App.js
@@ -3,10 +3,16 @@ import './App.css';
 function App() {
   return (
       <div className="todo"> 
-        <h1>Tasks</h1>
-          <div className="flex-container">
-            <h2>date</h2>
+        <div className="container">
+          <div className="tasks-box">
+            <h2>Date</h2>
+              <span><input type="text" name="task" id="add-task-box" placeholder="Add task"></input>
+              <button id="add-task-button">Add Task</button></span>
           </div>
+
+          <div className="calender-box">
+          </div>
+        </div>
       </div>
   )
 }
